920b7035f288a82a340fc24e4c62ea5dfec03997
aniketsingh10 <aniketsinghofficial144@gmail.com>
2024-02-09 21:35:08 +0530

    added api to fetch past month closing price data of top nifty indices using yfinance library

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -6,5 +6,10 @@ urlpatterns=[
     path('gainers/',views.gainers),
     path('losers/',views.losers),
     path('indices/',views.indices),
-
+    path('nse',views.fetch_nse_data),
+    path('bse',views.fetch_sensex_data),
+    path('bank',views.fetch_nifty_bank_data),
+    path('it',views.fetch_nifty_it_data),
+    path('finance',views.fetch_nifty_fin_data),
+    path('fmcg',views.fetch_nifty_fmcg_data),
 ]
