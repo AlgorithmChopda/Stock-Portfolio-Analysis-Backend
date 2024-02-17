ac81e24d37c6cafbf478670ed59fd5ea4865b8c7
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-17 18:53:00 +0530

    refactor: code lint

--- scrapper/views.py
+++ scrapper/views.py
@@ -11,24 +11,35 @@ import json
 import html
 import bs4
 
+
 # IPO Data
 def scrape_ipo_data(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    table_data = soup.find_all('figure', {'class': 'wp-block-table'})
+    soup = BeautifulSoup(html_content, "html.parser")
+    table_data = soup.find_all("figure", {"class": "wp-block-table"})
 
     ipo_list = []
 
     for table in table_data:
-        rows = table.find_all('tr')[1:8]  # Restrict to the top 7 rows (skip the header row)
+        rows = table.find_all("tr")[
+            1:8
+        ]  # Restrict to the top 7 rows (skip the header row)
 
         for row in rows:
-            cells = row.find_all('td')
+            cells = row.find_all("td")
 
             ipo_data = {
                 "Name": cells[0].get_text().strip(),
                 "Date": cells[1].get_text().strip(),
-                "IPOGMP": html.unescape(cells[3].get_text().strip()) if len(cells) > 3 else None,
-                "IPOPrice": html.unescape(cells[4].get_text().strip()) if len(cells) > 4 else None,
+                "IPOGMP": (
+                    html.unescape(cells[3].get_text().strip())
+                    if len(cells) > 3
+                    else None
+                ),
+                "IPOPrice": (
+                    html.unescape(cells[4].get_text().strip())
+                    if len(cells) > 4
+                    else None
+                ),
                 "Gain": cells[5].get_text().strip() if len(cells) > 5 else None,
             }
 
@@ -40,10 +51,11 @@ def scrape_ipo_data(html_content):
     return {"IPOs": ipo_list}
 
 
-
 def ipo_api(request):
-    screener = requests.get('https://ipowatch.in/ipo-grey-market-premium-latest-ipo-gmp/')
-    
+    screener = requests.get(
+        "https://ipowatch.in/ipo-grey-market-premium-latest-ipo-gmp/"
+    )
+
     if screener.status_code == 200:
         html_content = screener.text
         ipo_data = scrape_ipo_data(html_content)
@@ -54,69 +66,64 @@ def ipo_api(request):
         return HttpResponse("Failed to fetch data", status=500)
 
 
-
-    
 def gainers(request):
-        screener = requests.get('https://ticker.finology.in/market')
-        screener_soup = bs4.BeautifulSoup(screener.text, 'html.parser')
+    screener = requests.get("https://ticker.finology.in/market")
+    screener_soup = bs4.BeautifulSoup(screener.text, "html.parser")
 
-        table_data = screener_soup.find_all('table', {'class': 'screenertable'})
+    table_data = screener_soup.find_all("table", {"class": "screenertable"})
 
-        # Check if there are at least 5 tables with the specified class
-        if len(table_data) >= 5:
-            target_table = table_data[4]  # Extract the 5th table
-            rows = target_table.find_all('tr')[1:8]
+    # Check if there are at least 5 tables with the specified class
+    if len(table_data) >= 5:
+        target_table = table_data[4]  # Extract the 5th table
+        rows = target_table.find_all("tr")[1:8]
 
-            gainers_list = []
+        gainers_list = []
 
-            for row in rows:
-                cells = row.find_all('td')
+        for row in rows:
+            cells = row.find_all("td")
 
-                if cells:
-                    gainer_data = {
-                        "Name": cells[0].get_text().strip(),
-                        "Price": float(cells[1].get_text().strip()),
-                        "Change": cells[2].get_text().strip()
-                    }
-                    gainers_list.append(gainer_data)
+            if cells:
+                gainer_data = {
+                    "Name": cells[0].get_text().strip(),
+                    "Price": float(cells[1].get_text().strip()),
+                    "Change": cells[2].get_text().strip(),
+                }
+                gainers_list.append(gainer_data)
 
-            response_data = {"gainers": gainers_list}
-            return JsonResponse(response_data, json_dumps_params={'indent': 2})
-        else:
-            return JsonResponse({"error": "Not enough tables with the specified class."})
-
+        response_data = {"gainers": gainers_list}
+        return JsonResponse(response_data, json_dumps_params={"indent": 2})
+    else:
+        return JsonResponse({"error": "Not enough tables with the specified class."})
 
 
 def losers(request):
-        screener = requests.get('https://ticker.finology.in/market')
-        screener_soup = bs4.BeautifulSoup(screener.text, 'html.parser')
+    screener = requests.get("https://ticker.finology.in/market")
+    screener_soup = bs4.BeautifulSoup(screener.text, "html.parser")
 
-        table_data = screener_soup.find_all('table', {'class': 'screenertable'})
+    table_data = screener_soup.find_all("table", {"class": "screenertable"})
 
-        # Check if there are at least 5 tables with the specified class
-        if len(table_data) >= 6:
-            target_table = table_data[5]  # Extract the 5th table
-            rows = target_table.find_all('tr')[1:8]
+    # Check if there are at least 5 tables with the specified class
+    if len(table_data) >= 6:
+        target_table = table_data[5]  # Extract the 5th table
+        rows = target_table.find_all("tr")[1:8]
 
-            losers_list = []
+        losers_list = []
 
-            for row in rows:
-                cells = row.find_all('td')
+        for row in rows:
+            cells = row.find_all("td")
 
-                if cells:
-                    gainer_data = {
-                        "Name": cells[0].get_text().strip(),
-                        "Price": float(cells[1].get_text().strip()),
-                        "Change": cells[2].get_text().strip()
-                    }
-                    losers_list.append(gainer_data)
+            if cells:
+                gainer_data = {
+                    "Name": cells[0].get_text().strip(),
+                    "Price": float(cells[1].get_text().strip()),
+                    "Change": cells[2].get_text().strip(),
+                }
+                losers_list.append(gainer_data)
 
-            response_data = {"losers": losers_list}
-            return JsonResponse(response_data, json_dumps_params={'indent': 2})
-        else:
-            return JsonResponse({"error": "Not enough tables with the specified class."})
-
-
+        response_data = {"losers": losers_list}
+        return JsonResponse(response_data, json_dumps_params={"indent": 2})
+    else:
+        return JsonResponse({"error": "Not enough tables with the specified class."})
 
 
 # Indices data
@@ -161,38 +168,39 @@ def losers(request):
 #                     "change": float(change.get_text().strip().replace(",", "")),
 #                     "time": time.get_text().strip()
 #                 })
-            
+
 
 #     response_data = {"indices": indices_data}
 #     print(response_data)
 #     return JsonResponse(response_data)
 
+
 def indices(request):
-    url = 'https://in.tradingview.com/markets/indices/quotes-major/'
+    url = "https://in.tradingview.com/markets/indices/quotes-major/"
     response = requests.get(url)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find('table', {'class': 'table-Ngq2xrcG'})
+        soup = BeautifulSoup(response.text, "html.parser")
+        table = soup.find("table", {"class": "table-Ngq2xrcG"})
         indices_data = []
 
-        for row in table.find_all('tr'):
-            cells = row.find_all('td')
+        for row in table.find_all("tr"):
+            cells = row.find_all("td")
             if cells:
                 name = cells[0].get_text().strip()
-                price_str = cells[1].get_text().replace(',', '').strip()
-                percent_change_str = cells[2].get_text().replace('%', '').strip()
-                change_str = cells[3].get_text().replace(',', '').strip()
-                high_str = cells[4].get_text().replace(',', '').strip()
-                low_str = cells[5].get_text().replace(',', '').strip()
+                price_str = cells[1].get_text().replace(",", "").strip()
+                percent_change_str = cells[2].get_text().replace("%", "").strip()
+                change_str = cells[3].get_text().replace(",", "").strip()
+                high_str = cells[4].get_text().replace(",", "").strip()
+                low_str = cells[5].get_text().replace(",", "").strip()
                 tech_rating = cells[6].get_text().strip()
 
                 # Replace non-breaking hyphen with regular hyphen
-                price_str = price_str.replace('−', '-')
-                percent_change_str = percent_change_str.replace('−', '-')
-                change_str = change_str.replace('−', '-')
-                high_str = high_str.replace('−', '-')
-                low_str = low_str.replace('−', '-')
+                price_str = price_str.replace("−", "-")
+                percent_change_str = percent_change_str.replace("−", "-")
+                change_str = change_str.replace("−", "-")
+                high_str = high_str.replace("−", "-")
+                low_str = low_str.replace("−", "-")
 
                 # Convert strings to float
                 price = float(price_str)
@@ -201,37 +209,39 @@ def indices(request):
                 high = float(high_str)
                 low = float(low_str)
 
-                indices_data.append({
-                    "name": name,
-                    "price": price,
-                    "percentChange": percent_change,
-                    "change": change,
-                    "high": high,
-                    "low": low,
-                    "tech_rating": tech_rating
-                })
+                indices_data.append(
+                    {
+                        "name": name,
+                        "price": price,
+                        "percentChange": percent_change,
+                        "change": change,
+                        "high": high,
+                        "low": low,
+                        "tech_rating": tech_rating,
+                    }
+                )
 
         response_data = {"indices": indices_data}
         return JsonResponse(response_data)
 
     else:
-        return JsonResponse({"error": f"Failed to fetch data. Status code: {response.status_code}"})
-
+        return JsonResponse(
+            {"error": f"Failed to fetch data. Status code: {response.status_code}"}
+        )
 
 
 # Data fetch from yfinance
 def fetch_stock_data(request, ticker_symbol):
-    start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d")
 
-    data = yf.download(ticker_symbol, start=start_date, end=datetime.now().strftime('%Y-%m-%d'))['Close']
+    data = yf.download(
+        ticker_symbol, start=start_date, end=datetime.now().strftime("%Y-%m-%d")
+    )["Close"]
     data = data.astype(int)
 
     result = []
     for date, close in data.items():
-        result.append({
-            "date": date.strftime('%Y-%m-%d'),
-            "Nifty": int(close)
-        })
+        result.append({"date": date.strftime("%Y-%m-%d"), "Nifty": int(close)})
 
     return JsonResponse({"nifty": result}, safe=False)
 
@@ -241,25 +251,30 @@ def fetch_stock_data(request, ticker_symbol):
 def fetch_nse_data(request):
     return fetch_stock_data(request, "^NSEI")
 
+
 @csrf_exempt
 @require_http_methods(["GET"])
 def fetch_sensex_data(request):
     return fetch_stock_data(request, "^BSESN")
+
 
 @csrf_exempt
 @require_http_methods(["GET"])
 def fetch_nifty_bank_data(request):
     return fetch_stock_data(request, "^NSEBANK")
 
+
 @csrf_exempt
 @require_http_methods(["GET"])
 def fetch_nifty_it_data(request):
     return fetch_stock_data(request, "^CNXIT")
 
+
 @csrf_exempt
 @require_http_methods(["GET"])
 def fetch_nifty_fin_data(request):
     return fetch_stock_data(request, "NIFTY_FIN_SERVICE.NS")
+
 
 @csrf_exempt
 @require_http_methods(["GET"])
